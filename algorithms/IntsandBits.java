bfb7ae0b5c49c3a408a0fc77ca668e36ee1cc1aa
Nuh Mohamud <nuh.mohamud@gmail.com>
2018-02-15 18:02:08 -0800

    more integer solutions i've made

--- algorithms/IntsandBits.java
+++ algorithms/IntsandBits.java
@@ -1,10 +1,38 @@
 package algorithms;
-
+import java.util.*;
 public class IntsandBits {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
+	
+    public int reverse(int x) {
+        int rev = 0;
+        int prev = 0;
+        boolean isNeg = x < 0;
+        x = Math.abs(x);
+        while(x > 0) {
+            rev = rev*10 + x%10;            
+            if((rev - x%10)/10 != prev) {
+                return 0;
+            }
+            prev = rev;
+            x /= 10;
+
+        }
+        
+        return (isNeg) ? -rev : rev;
+    }
+    
+    public boolean isPalindrome(int x) {
+        int fin = x;
+        int prev = 0;
+        while(x > 0) {
+            prev = prev*10 + x%10;
+            x /= 10;
+        }
+        return (fin == prev);
+        
+    }
 
 }
